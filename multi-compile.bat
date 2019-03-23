2c844e81f395ed030b92a50d4c95941e0a5fdf1f
Andreas Schuettler <aschuettler@web.de>
2019-03-23 17:19:02 +0100

    add executables.bat for java11 environment; modify run.bat and multi-compile.bat to call executables.bat

--- multi-compile.bat
+++ multi-compile.bat
@@ -1,4 +1,7 @@
 @echo off
+echo "--- SET JAVA11 ENVIRONMENT ---"
+call executables.bat
+
 echo "--- COMPILATION & PACKAGING ---"
 
 echo " > creating clean directories"
@@ -12,13 +15,13 @@ mkdir mods
 echo " > multi-compiling modules"
 rem spark is required as an automatic module, so copy it to mods
 copy libs\spark-core-*.jar mods\
-javac --module-path mods --module-source-path "./*/src/main/java" -d classes --module monitor
+%javac% --module-path mods --module-source-path "./*/src/main/java" -d classes --module monitor
 
 echo " > packaging modules"
-jar --create --file mods/monitor.observer.jar -C classes/monitor.observer .
-jar --create --file mods/monitor.observer.alpha.jar -C classes/monitor.observer.alpha .
-jar --create --file mods/monitor.observer.beta.jar -C classes/monitor.observer.beta .
-jar --create --file mods/monitor.statistics.jar -C classes/monitor.statistics .
-jar --create --file mods/monitor.persistence.jar -C classes/monitor.persistence .
-jar --create --file mods/monitor.rest.jar -C classes/monitor.rest .
-jar --create --file mods/monitor.jar --main-class monitor.Main -C classes/monitor .
+%jar% --create --file mods/monitor.observer.jar -C classes/monitor.observer .
+%jar% --create --file mods/monitor.observer.alpha.jar -C classes/monitor.observer.alpha .
+%jar% --create --file mods/monitor.observer.beta.jar -C classes/monitor.observer.beta .
+%jar% --create --file mods/monitor.statistics.jar -C classes/monitor.statistics .
+%jar% --create --file mods/monitor.persistence.jar -C classes/monitor.persistence .
+%jar% --create --file mods/monitor.rest.jar -C classes/monitor.rest .
+%jar% --create --file mods/monitor.jar --main-class monitor.Main -C classes/monitor .
--- run.bat
+++ run.bat
@@ -1,12 +1,13 @@
 @echo off
 echo ""
+echo "--- SET JAVA11 ENVIRONMENT ---"
+call executables.bat
 echo "--- LAUNCH ---"
-
 if "%1" == "mvn" goto maven
 echo " > run monitor"
 echo ""
 rem the classpath is needed for Spark's dependencies
-java --module-path mods --class-path "libs/*" --module monitor
+%java% --module-path mods --class-path "libs/*" --module monitor
 goto end
 
 :maven
@@ -14,6 +15,6 @@ echo " > run monitor from Maven build"
 echo ""
 rem This version runs the application when built with Maven.
 rem the classpath is needed for Spark's dependencies
-java --module-path mods-mvn --class-path "libs/*" --module monitor/monitor.Main
+%java% --module-path mods-mvn --class-path "libs/*" --module monitor/monitor.Main
 
 :end
